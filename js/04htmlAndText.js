d9819206aab4cda9c8778c4c7fef8708360f7604
xiaodoufu <501804292@qq.com>
2017-09-11 11:07:43 +0800

    链式操作和动态增加类样式

---
+++ js/04htmlAndText.js
@@ -0,0 +1,6 @@
+$(function(){
+    //获取页面中的div   innerHTML=""   会编译html标签
+    //$("#myDiv").html("<img src='../images/cat.jpg'  height='50px' width='50px'/>");
+  //  innerText   文本内容
+    $("#myDiv").text("<img src='../images/cat.jpg'  height='50px' width='50px'/>");
+})
---
+++ js/06addClass.js
@@ -0,0 +1,8 @@
+$(function(){
+    //获取页面中所有的div动态增加类样式  之前js中使用的是 className="类名"
+    $("div").hover(function(){
+        $(this).addClass("haha"); /*增加样式*/
+    },function(){
+        $(this).removeClass("haha");/*删除样式*/
+    })
+})
