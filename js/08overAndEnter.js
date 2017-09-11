4f4279395fcb7b95d0e126ca79848edd4141d259
xiaodoufu <501804292@qq.com>
2017-09-11 12:06:22 +0800

    mouseover和mouseenter

---
+++ js/08overAndEnter.js
@@ -0,0 +1,10 @@
+$(function(){
+
+    //获取div的鼠标移入事件
+   /* $("#father").mouseover(function(){
+         $(this).css({"border":"1px solid red"});
+    })*/
+    $("#father").mouseenter(function(){
+        $(this).css({"border":"1px solid red"});
+    })
+})
